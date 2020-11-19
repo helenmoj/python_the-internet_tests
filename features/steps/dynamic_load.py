17db52beb64c0d1f52ff64b65174554f9adb01d7
Helen Mojsak <helensroberts79@gmail.com>
2020-11-19 19:12:48 +0000

    third commit

--- features/steps/dynamic_load.py
+++ features/steps/dynamic_load.py
@@ -3,23 +3,40 @@ from selenium import webdriver
 from behave import given
 import time
 
+from IPython import embed
+
 from behave_webdriver.steps import *
 
 
-from IPython import embed
+#from IPython import embed
+import IPython
 
 @given('I reach the internet page')
 def step_impl(context):
-   context.browser.get("https://the-internet.herokuapp.com")
+    context.browser.get("https://the-internet.herokuapp.com")
 
 
 @given('I select dynamic loading')
 def select_dym_load(context):
     context.browser.find_element_by_xpath("//*[@id='content']/ul/li[14]/a").click()
 
+
 @given('I click example two')
 def select_exam_two(context):
     context.browser.find_element_by_xpath("//*[@id='content']/div/a[2]").click()
+
+@given('I see the text \'Example 2: Element rendered after the fact\' is displayed')
+def text(context):
+    status = context.browser.find_element_by_xpath("//*[@id='content']/div/h3").is_displayed()
+    assert status is True
+
+@given('I select the start button')
+def start_button(context):
+    context.browser.find_element_by_id('start').click()
+
+
+
+
 
 
 
